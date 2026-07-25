c39390676db4078d74708da98ab8c5bbb4f63a4b
Ayush01010101 <ayushawachar0989@gmail.com>
2026-07-25 20:30:46 +0530

    twopointers question

--- TwoPointers/SumofTargetedNumbers.ts
+++ TwoPointers/SumofTargetedNumbers.ts
@@ -1,10 +1,28 @@
-const array = [1, 2, 2, 3, 4]
-
+const array = [5, 22, 33, 45, 101]
 function sumoftargetedNumber(targedsum: number = 4) {
 
+  let left = 0
+  let right = array.length - 1
+
+  while (left <= right) {
+
+    if ((array[left] + array[right]) < targedsum) {
+      left = left + 1
+    } else if ((array[left] + array[right]) == targedsum) {
+      return [left, right]
+    } else {
+      right = right - 1
+    }
+
+  }
+  return "Invalid Array Provided"
+}
+
+const result = sumoftargetedNumber(146)
 
 
+if (typeof result == 'object') {
 
-
+  console.log(array[result[0]], array[result[1]])
 }
 
